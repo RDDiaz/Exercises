78116184381da4171e7cc750ca7279ede5daa7ec
Richard D Diaz <richarddanieldiaz@gmail.com>
2017-10-28 17:07:00 -0400

    Add files via upload

---
+++ Java/FizzBuzz/src/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,28 @@
+
+package fizzbuzz;
+
+/**
+ *
+ * @author Richard Diaz
+ * FizzBuzz solution
+ */
+public class FizzBuzz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int counter = 1; counter <= 100; counter++){
+            
+            if(counter % 3 == 0 && counter % 5 == 0)
+                System.out.println("FizzBuzz");
+            else if(counter % 3 == 0)
+                System.out.println("Fizz");
+            else if(counter % 5 == 0 )
+                System.out.println("Buzz");
+            else
+            System.out.println(counter);
+        }
+    }
+    
+}
